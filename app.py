42a3268677702319c60a72192bccb34f09e6344d
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-13 20:21:25 +0100

    upload

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
-
 # Importing required packages
 import streamlit as st
 import openai
 import uuid
 import time
-
+import pandas as pd
+import io
 from openai import OpenAI
+
+# Initialize OpenAI client
 client = OpenAI()
 
-#MODEL = "gpt-3.5-turbo"
-#MODEL = "gpt-3.5-turbo-0301"
-#MODEL = "gpt-3.5-turbo-0613"
-#MODEL = "gpt-3.5-turbo-1106"
-#MODEL = "gpt-3.5-turbo-16k"
-#MODEL = "gpt-3.5-turbo-16k-0613"
-#MODEL = "gpt-4"
-#MODEL = "gpt-4-0613"
-#MODEL = "gpt-4-0613"
-#MODEL = "gpt-4-32k-0613"
+# Your chosen model
 MODEL = "gpt-4-1106-preview"
-#MODEL = "gpt-4-vision-preview"
 
+# Initialize session state variables
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
 
@@ -33,6 +26,7 @@ if "messages" not in st.session_state:
 if "retry_error" not in st.session_state:
     st.session_state.retry_error = 0
 
+# Set up the page
 st.set_page_config(page_title="Lección #1: El éxito en la innovación")
 st.sidebar.title("Lección #1")
 st.sidebar.divider()
@@ -40,27 +34,48 @@ st.sidebar.markdown("Por Pedro Aragón", unsafe_allow_html=True)
 st.sidebar.markdown("Asistente GPT")
 st.sidebar.divider()
 
+# File uploader for CSV, XLS, XLSX
+uploaded_file = st.file_uploader("Upload your file", type=["csv", "xls", "xlsx"])
+
+if uploaded_file is not None:
+    # Determine the file type
+    file_type = uploaded_file.type
+
+    try:
+        # Read the file into a Pandas DataFrame
+        if file_type == "text/csv":
+            df = pd.read_csv(uploaded_file)
+        elif file_type in ["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]:
+            df = pd.read_excel(uploaded_file)
+
+        # Convert DataFrame to JSON
+        json_str = df.to_json(orient='records', indent=4)
+        file_stream = io.BytesIO(json_str.encode())
+
+        # Upload JSON data to OpenAI
+        file_response = client.files.create(file=file_stream, purpose='answers')
+        st.success("File uploaded successfully to OpenAI!")
+
+        # Optional: Display and Download JSON
+        st.text_area("JSON Output", json_str, height=300)
+        st.download_button(label="Download JSON", data=json_str, file_name="converted.json", mime="application/json")
+    
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+# Initialize OpenAI assistant
 if "assistant" not in st.session_state:
     openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-    # Load the previously created assistant
     st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT"])
-
-    # Create a new thread for this session
     st.session_state.thread = client.beta.threads.create(
-        metadata={
-            'session_id': st.session_state.session_id,
-        }
+        metadata={'session_id': st.session_state.session_id}
     )
- 
-# If the run is completed, display the messages
+
+# Display chat messages
 elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
-    # Retrieve the list of messages
     st.session_state.messages = client.beta.threads.messages.list(
         thread_id=st.session_state.thread.id
     )
-
-    # Display messages
     for message in reversed(st.session_state.messages.data):
         if message.role in ["user", "assistant"]:
             with st.chat_message(message.role):
@@ -68,50 +83,45 @@ elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == 
                     message_text = content_part.text.value
                     st.markdown(message_text)
 
+# Chat input
 if prompt := st.chat_input("How can I help you?"):
     with st.chat_message('user'):
         st.write(prompt)
 
-    # Add message to the thread
     st.session_state.messages = client.beta.threads.messages.create(
         thread_id=st.session_state.thread.id,
         role="user",
         content=prompt
     )
 
-    # Do a run to process the messages in the thread
     st.session_state.run = client.beta.threads.runs.create(
         thread_id=st.session_state.thread.id,
         assistant_id=st.session_state.assistant.id,
     )
     if st.session_state.retry_error < 3:
-        time.sleep(1) # Wait 1 second before checking run status
+        time.sleep(1)
         st.rerun()
-                    
-# Check if 'run' object has 'status' attribute
+
+# Handle run status
 if hasattr(st.session_state.run, 'status'):
-    # Handle the 'running' status
     if st.session_state.run.status == "running":
         with st.chat_message('assistant'):
             st.write("Thinking ......")
         if st.session_state.retry_error < 3:
-            time.sleep(1)  # Short delay to prevent immediate rerun, adjust as needed
+            time.sleep(1)
             st.rerun()
 
-    # Handle the 'failed' status
     elif st.session_state.run.status == "failed":
         st.session_state.retry_error += 1
         with st.chat_message('assistant'):
             if st.session_state.retry_error < 3:
                 st.write("Run failed, retrying ......")
-                time.sleep(3)  # Longer delay before retrying
+                time.sleep(3)
                 st.rerun()
             else:
                 st.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
 
-    # Handle any status that is not 'completed'
     elif st.session_state.run.status != "completed":
-        # Attempt to retrieve the run again, possibly redundant if there's no other status but 'running' or 'failed'
         st.session_state.run = client.beta.threads.runs.retrieve(
             thread_id=st.session_state.thread.id,
             run_id=st.session_state.run.id,
